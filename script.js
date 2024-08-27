145c2a451584fa6a371b0df30405895023293b05
Dennis <DennisS010101@gmail.com>
2024-08-27 16:18:05 -0400

    feat: add playRound function
    Implemented the playRound function to handle a single round of the
    game. It retrieves the user's choice and the computer's choice, checks
    for invalid inputs, determines the outcome (win, lose, or tie), and
    updates the scores accordingly.

--- script.js
+++ script.js
@@ -20,4 +20,27 @@ function getUserChoice() {
   return formattedUserChoice;
 }
 
-console.log(getUserChoice());
+function playRound(userScore, computerScore) {
+  let userChoice = getUserChoice();
+  let computerChoice = generateComputerChoice();
+  if (userChoice !== "Rock" && userChoice !== "Paper" && userChoice !== "Scissors") {
+    console.log("Enter a valid choice!");
+  }
+  else if (userChoice === "Rock" && computerChoice === "Rock" || 
+    userChoice === "Paper" && computerChoice === "Paper" || 
+    userChoice === "Scissors" && computerChoice === "Scissors") {
+      console.log("It's a tie!")
+    }
+  else if (userChoice === "Rock" && computerChoice === "Scissors" || 
+    userChoice === "Paper" && computerChoice === "Rock" || 
+    userChoice === "Scissors" && computerChoice === "paper") {
+      console.log(`You won! ${userChoice} beats ${computerChoice}`);
+      userScore++;
+    }
+  else {
+    console.log(`You lost! ${computerChoice} beats ${userChoice}`)
+    computerScore++;
+  }
+}
+
+playRound();
